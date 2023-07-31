81f31d9377aa34b230dd3cce372f4d4bf37dd4fe
suhyuns22 <138412534+suhyuns22@users.noreply.github.com>
2023-07-31 13:34:51 +0900

    230731 폼전송

---
+++ 230731_form/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+//body-parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+//router
+app.get("/", (req, res) => {
+  //   res.send("Hello");
+  res.render("index", { title: "폼 실습" });
+});
+
+app.get("/getForm", (req, res) => {
+  console.log(req.query);
+  res.render("result", {
+    title: "GET 요청 폼 결과 확인하기",
+    userInfo: req.query,
+  });
+});
+
+app.post("/postForm", (req, res) => {
+  console.log(req.body);
+  res.render("result", {
+    title: "POST 요청 폼 결과 확인하기",
+    userInfo: req.body,
+  });
+});
+
+app.put("/", (req, res) => {
+  console.log(req.body);
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
