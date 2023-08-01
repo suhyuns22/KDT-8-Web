e259303a1783e64164af78ca36ebe32da85383eb
suhyuns22 <138412534+suhyuns22@users.noreply.github.com>
2023-08-01 21:40:53 +0900

    0801실습

---
+++ 230801/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+//body-parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+//router
+app.get("/", (req, res) => {
+  res.render("login");
+});
+
+app.post("/axios", (req, res) => {
+  console.log("back", req.body);
+  res.send(req.body);
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
