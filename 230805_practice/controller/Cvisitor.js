501d380df22ec39176f529781afd56123f7f05e1
suhyuns22 <138412534+suhyuns22@users.noreply.github.com>
2023-08-07 13:49:23 +0900

    230805 실습 풀이

--- 230805_practice/controller/Cvisitor.js
+++ 230805_practice/controller/Cvisitor.js
@@ -1,16 +1,37 @@
-const Visitor = require("../model/Visitor");
+const Visitor = require("../model/MVisitor");
 
-exports.main = (req, res) => {
-  res.render("index");
-};
-
+//전체 방명록 조회
 exports.getVisitors = (req, res) => {
-  //   console.log(Visitor.getVisitors());
-  //   res.render("visitor", { data: Visitor.getVisitors() });
-
-  //mysql 연결 후
   Visitor.getVisitors((result) => {
-    console.log("Cvisitor.js:", result);
     res.render("visitor", { data: result });
+  });
+};
+//방명록 하나 조회
+exports.getVisitor = (req, res) => {
+  Visitor.getVisitor(req.query.id, (result) => {
+    res.render("visitor", { data: result });
+  });
+};
+//방명록 하나 추가
+exports.postVisitor = (req, res) => {
+  Visitor.postVisitor(req.body, (result) => {
+    res.send({
+      result: true,
+      id: result.insertId,
+      name: req.body.name,
+      comment: req.body.comment,
+    });
+  });
+};
+//방명록 하나 수정
+exports.patchVisitor = (req, res) => {
+  Visitor.patchVisitor(req.body, () => {
+    res.send({ result: true });
+  });
+};
+//방명록 하나 삭제
+exports.deleteVisitor = (req, res) => {
+  Visitor.deleteVisitor(req.body, () => {
+    res.send({ result: true });
   });
 };
--- 230805_practice/index.js
+++ 230805_practice/index.js
@@ -3,19 +3,19 @@ const app = express();
 const PORT = 8000;
 
 app.set("view engine", "ejs");
-app.use("/views", express.static(__dirname + "/views"));
-app.use("/static", express.static(__dirname + "/static"));
+app.set("views", "./views");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const indexRouter = require("./routes");
-app.use("/", indexRouter);
-
+//router 분리
 app.get("/", (req, res) => {
   res.render("index");
 });
+const router = require("./routes");
+app.use("/visitor", router);
 
-app.get("*", (req, res) => {
+//오류처리
+app.use("*", (req, res) => {
   res.render("404");
 });
 
