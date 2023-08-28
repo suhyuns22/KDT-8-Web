25bdcd714f0a02baead644f7df9be16c65fc06c7
suhyuns22 <138412534+suhyuns22@users.noreply.github.com>
2023-08-28 10:22:00 +0900

    230828 쿠키&세션

---
+++ 19_jwt-finish/models/User.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+
+const Model = (sequelize) => {
+    return sequelize.define('user', {
+        //컬럼 정의
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false, //NOT NULL
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        userid: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING(10),
+            allowNull: false,
+        },
+        pw: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+        },
+        str_id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+        },
+    });
+};
+
+module.exports = Model;
